6234af0d786e9fbfe3917a7887480ff6e5d0d93d
Gosha Gaponov <egoriks1994@gmail.com>
2022-02-23 22:29:09 +0200

    create css header

--- #src/js/script.js
+++ #src/js/script.js
@@ -2,22 +2,6 @@
 
 // START ===== функция проверяет может ли браузер отображатьь webp формат изображений ===== 
 
-function testWebP(callback) {
 
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-    }
-    
-    testWebP(function (support) {
-    
-        if (support == true) {
-            document.querySelector('body').classList.add('webp');
-        }else{
-            document.querySelector('body').classList.add('no-webp');
-    }
-    });
 
 // END ===== функция проверяет может ли браузер отображатьь webp формат изображений ===== 
