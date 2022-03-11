668fe3f3a4a467b484562bff3c016a02e7e23bf4
Gosha Gaponov <egoriks1994@gmail.com>
2022-03-11 19:10:27 +0200

    fix: cursor:pointer, closed burger menu with click on link etc

--- #src/js/burger.js
+++ #src/js/burger.js
@@ -1,16 +1,25 @@
-// START ===== Menu burger ===== 
+// START ===== Menu burger =====
+const menuBurgerBody = document.querySelector(".menu__body");
+const menuBurgerIcon = document.querySelector(".action-header__icon-menu");
+const menuItems = document.querySelectorAll(".menu__item");
+const menuBody = document.querySelector(".menu__body");
 
-const menuBurgerIcon = document.querySelector('.action-header__icon-menu');
+for (item of menuItems) {
+    item.addEventListener("touchstart", () => {
+        if (menuBody.classList.contains("_active")) {
+            showMenuBurger();
+        }
+    });
+}
+
 if (menuBurgerIcon) {
-    const menuBurgerBody = document.querySelector('.menu__body');
-    menuBurgerIcon.addEventListener('touchstart', () => {
-        menuBurgerIcon.classList.toggle('_active');
-        menuBurgerBody.classList.toggle('_active');
-        document.body.classList.toggle('_lock');
-    })
-    // menuBurgerIcon.ontouchstart = () => {
-        
-    // }
+    menuBurgerIcon.addEventListener("touchstart", showMenuBurger);
+}
+
+function showMenuBurger() {
+    menuBurgerIcon.classList.toggle("_active");
+    menuBurgerBody.classList.toggle("_active");
+    document.body.classList.toggle("_lock");
 }
 
 // END ===== Menu burger =====
--- photo_site/js/script.js
+++ photo_site/js/script.js
@@ -20,56 +20,98 @@ function testWebP(callback) {
     });
 
 // END ===== функция проверяет может ли браузер отображатьь webp формат изображений ===== ; // нет ошибки
-// START ===== Menu burger ===== 
+// START ===== Menu burger =====
+const menuBurgerBody = document.querySelector(".menu__body");
+const menuBurgerIcon = document.querySelector(".action-header__icon-menu");
+const menuItems = document.querySelectorAll(".menu__item");
+const menuBody = document.querySelector(".menu__body");
 
-const menuBurgerIcon = document.querySelector('.action-header__icon-menu');
-if (menuBurgerIcon) {
-    const menuBurgerBody = document.querySelector('.menu__body');
-    menuBurgerIcon.addEventListener('touchstart', () => {
-        menuBurgerIcon.classList.toggle('_active');
-        menuBurgerBody.classList.toggle('_active');
-        document.body.classList.toggle('_lock');
-    })
-    // menuBurgerIcon.ontouchstart = () => {
-        
-    // }
+for (item of menuItems) {
+    item.addEventListener("touchstart", () => {
+        if (menuBody.classList.contains("_active")) {
+            showMenuBurger();
+        }
+    });
 }
 
-// END ===== Menu burger =====;
+if (menuBurgerIcon) {
+    menuBurgerIcon.addEventListener("touchstart", showMenuBurger);
+}
+
+function showMenuBurger() {
+    menuBurgerIcon.classList.toggle("_active");
+    menuBurgerBody.classList.toggle("_active");
+    document.body.classList.toggle("_lock");
+}
+
+// END ===== Menu burger =====
+;
 // START ===== Slider =====
 
+
+// START ===== values =====
 const nextBtn = document.querySelector(".slider__arrows-right");
 const prevBtn = document.querySelector(".slider__arrows-left");
 const sliderBody = document.querySelector(".slider__body");
-const sliderPaginationItems = document.querySelectorAll(".slider__dots-item");
-sliderPagination();
+let sliderPaginationItems = 0;
+const sliderDotsBody = document.querySelector('.slider__dots');
 
-function prevSlidePush() {
-    const lastSlide = document.querySelector(".slider__body-item:last-child");
-    const firstSlide = document.querySelector(".slider__body-item:first-child");
+// END ===== values =====
+
+
+// START ===== load default =====
+createPagination (sliderBody.children.length, 'slider__dots-item', 'div');
+
+// END ===== load default =====
+
+// START ===== load event =====
+prevBtn.addEventListener("click", () => {
+    prevSlidePush(".slider__body-item:last-child", ".slider__body-item:first-child")
+})
+nextBtn.addEventListener("click", () => {
+    nextSlidePush(".slider__body-item:first-child");
+})
+
+// END ===== load event =====
+
+
+// START ===== function =====
+function prevSlidePush(last, first) {
+    const lastSlide = document.querySelector(last);
+    const firstSlide = document.querySelector(first);
     sliderBody.insertBefore(lastSlide, firstSlide);
     sliderPagination();
 }
-function nextSlidePush() {
-    const firstSlide = document.querySelector(".slider__body-item:first-child");
+
+function nextSlidePush(first) {
+    const firstSlide = document.querySelector(first);
     sliderBody.append(firstSlide);
     sliderPagination();
 }
 
-prevBtn.addEventListener("click", prevSlidePush);
-nextBtn.addEventListener("click", nextSlidePush);
+
 
 function sliderPagination() {
-    let child = sliderBody.children[0].getAttribute("id");
+    let child = sliderBody.children[0].getAttribute("name");
     for (let i = 0; i < sliderPaginationItems.length; i++) {
         sliderPaginationItems[i].classList.remove("active-dots");
         if (i == (child - 1)) {
             sliderPaginationItems[i].classList.add("active-dots");
         }
     }
-    console.log(child)
 }
 
+
+function createPagination (numOfChild, classNameChild, typeOfChild) {   
+    for (let i = 1; i <= numOfChild; i++){
+        let sliderDot = document.createElement(typeOfChild);
+        sliderDot.classList.add(classNameChild);
+        sliderDotsBody.appendChild(sliderDot);
+    }
+    sliderPaginationItems = document.querySelectorAll(".slider__dots-item");
+    sliderPagination();
+}
+// END ===== function =====
 // END ===== Slider =====
 ;
 // START =====  ===== 
