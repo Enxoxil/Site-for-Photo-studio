6b7fcde181b8c4abc4229793a34a318bcbe9a3f8
Gosha Gaponov <egoriks1994@gmail.com>
2022-03-24 02:09:37 +0200

    fix: some bugs, feat: create section-price

--- photo_site/js/script.js
+++ photo_site/js/script.js
@@ -155,7 +155,8 @@ function initSliderPaginations() {
 // textBlock__button
 
 let btn = document.querySelector('.textBlock__button').onclick = () => {
-    document.querySelector('.textBlock__content').classList.toggle('_fullText')
+    document.querySelector('.textBlock__content').classList.toggle('_fullText');
+    document.querySelector('.textBlock__button').classList.toggle('_activeButtonSpoiler');
 };
 // START =====  ===== 
 
