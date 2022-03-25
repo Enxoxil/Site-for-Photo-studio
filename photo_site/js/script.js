c8f8e1ce671f9788813feb4f64bfae4385ee837c
Gosha Gaponov <egoriks1994@gmail.com>
2022-03-26 00:13:42 +0200

    feat: finish css for article sections

--- photo_site/js/script.js
+++ photo_site/js/script.js
@@ -157,9 +157,9 @@ function initSliderPaginations() {
 // _fullText
 // textBlock__button
 
-let btn = document.querySelector('.textBlock__button').onclick = () => {
-    document.querySelector('.textBlock__content').classList.toggle('_fullText');
-    document.querySelector('.textBlock__button').classList.toggle('_activeButtonSpoiler');
+let btn = document.querySelector('.article__button').onclick = () => {
+    document.querySelector('.article__content').classList.toggle('_fullText');
+    document.querySelector('.article__button').classList.toggle('_activeButtonSpoiler');
 };
 // START =====  ===== 
 
