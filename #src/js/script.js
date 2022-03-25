25474df7a1aef46c9c9a4a47fd3e534a89277b60
Gosha Gaponov <egoriks1994@gmail.com>
2022-03-25 14:31:33 +0200

    create gitignore file

--- #src/js/script.js
+++
@@ -1,9 +0,0 @@
-@@include('webpCheck.js'); // нет ошибки
-@@include('burger.js');
-@@include('slider-logical.js');
-@@include('spoiler.js');
-// START =====  ===== 
-
-
-
-// END =====  ===== 
--- #src/js/spoiler.js
+++
@@ -1,7 +0,0 @@
-// _fullText
-// textBlock__button
-
-let btn = document.querySelector('.textBlock__button').onclick = () => {
-    document.querySelector('.textBlock__content').classList.toggle('_fullText');
-    document.querySelector('.textBlock__button').classList.toggle('_activeButtonSpoiler');
-}
