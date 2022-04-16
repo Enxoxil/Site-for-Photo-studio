9696475050ca5dc037c84fa690d8bb7dd0ec8d87
Gosha Gaponov <egoriks1994@gmail.com>
2022-04-17 00:35:22 +0200

    feat: adding modal-feedback

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,8 +1,13 @@
+// START ===== INCLUDES ===== 
+
 @@include('webpCheck.js'); // нет ошибки
 @@include('burger.js');
 @@include('sliderLogic.js');
 @@include('spoiler.js');
 @@include('smoothScroll.js');
+
+// END ===== INCLUDES ===== 
+
 // START =====  ===== 
 
 
