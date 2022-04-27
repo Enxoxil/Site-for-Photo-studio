24a87460ac7a1a2df75db86cd35464a361078fef
Gosha Gaponov <egoriks1994@gmail.com>
2022-04-27 23:19:50 +0200

    refactor: edit some function according functional style

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,27 +1,26 @@
-// START ===== INCLUDES =====
+// START ===== init =====
+
 const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
     navigator.userAgent
 )
 
+getAllForms();
 
+// END ===== init =====
 
-
-@@include('webpCheck.js'); // нет ошибки
-@@include('burger.js');
-@@include('sliderLogic.js');
-@@include('spoiler.js');
-@@include('smoothScroll.js');
-@@include('formHandler.js');
-@@include('showFeedbackModal.js');
-@@include('closeModalFieldHandler.js');
+@@include('webpCheck.js')
+@@include('burger.js')
+@@include('sliderLogic.js')
+@@include('spoiler.js')
+@@include('smoothScroll.js')
+@@include('formHandler.js')
+@@include('showModalHandlers.js')
+@@include('closeModalFieldHandler.js')
 @@include('closeModalEscHandler.js')
 @@include('closeModalBtnHandler.js')
 
-// END ===== INCLUDES =====
-
-// START ===== init =====
-
-getAllForms();
 
 
-// END =====  =====
+
+
+
--- #src/js/spoiler.js
+++ #src/js/spoiler.js
@@ -1,24 +1,31 @@
 // START ===== ToggleSpoiler =====
 
-// START ===== variables =====
-
-const toggleSpoilerButtons = document.querySelectorAll(".article__button");
-
-// END ===== variables =====
-
-// START ===== loops =====
-
-for (let toggleSpoilerButton of toggleSpoilerButtons) {
-    toggleSpoilerButton.addEventListener("click", function () {
-        this.parentElement.parentElement
-            .querySelector(".article__content")
-            .classList.toggle("_fullText");
-        this.parentElement.parentElement
-            .querySelector(".article__button")
-            .classList.toggle("_activeButtonSpoiler");
-    });
+function getSubscribeToggleSpoilerButtons() {
+    return document.querySelectorAll(".article__button");
 }
 
-// END ===== loops =====
+function toggleSpoilerButtonHandler() {
+    this.parentElement.parentElement
+        .querySelector(".article__content")
+        .classList.toggle("_fullText");
+    this.parentElement.parentElement
+        .querySelector(".article__button")
+        .classList.toggle("_activeButtonSpoiler");
+}
+
+function subscribeToggleSpoiler() {
+    for (let toggleSpoilerButton of getSubscribeToggleSpoilerButtons()) {
+        toggleSpoilerButton.addEventListener(
+            "click",
+            toggleSpoilerButtonHandler
+        );
+    }
+}
+
+function initSubscribeToggleSpoiler() {
+    subscribeToggleSpoiler();
+}
+
+initSubscribeToggleSpoiler();
 
 // END ===== ToggleSpoiler =====
