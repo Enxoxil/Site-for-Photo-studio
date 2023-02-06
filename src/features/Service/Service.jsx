7d4e3aac3456c0d69b07ac3cc1b6a19fc59d2578
Yehor Haponov <egoriks1994@gmail.com>
2023-02-07 01:54:19 +0200

    refactor: fix class name in Service.jsx

--- src/features/Service/Service.jsx
+++ src/features/Service/Service.jsx
@@ -13,77 +13,77 @@ class Service extends Component {
 
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img1.png" alt="Фотосессия Лав Стори"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Фотосессия Лав Стори
                                 </p>
                             </li>
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img2.png" alt="Фотосессия для подростков"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Фотосессия для подростков
                                 </p>
                             </li>
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img3.png" alt="Парная фотосессия"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Парная фотосессия
                                 </p>
                             </li>
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img4.png" alt="Фотосессия для парней"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Фотосессия для парней
                                 </p>
                             </li>
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img5.png" alt="Семейная фотосессия (дети +14)"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Семейная фотосессия (дети +14)
                                 </p>
                             </li>
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img6.png" alt="Групповая фотосессия"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Групповая фотосессия
                                 </p>
                             </li>
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img7.png" alt="Фотосессия Лук Бук"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Фотосессия для Лук Бук
                                 </p>
                             </li>
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img8.png" alt="Фотосессия для девушек"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Фотосессия для девушек
                                 </p>
                             </li>
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img9.png" alt="Фотосессия для беременных"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Фотосессия для беременных
                                 </p>
                             </li>
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img10.png" alt="Фотосессия с животными"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Фотосессия с животными
                                 </p>
                             </li>
                             <li className={classes.service__content_item}>
                                 <img src="img/service-img11.png" alt="Фотосессия 'Деловой стиль'"
-                                     className={classes.service__content_item}/>
+                                     className={classes.service__content_img}/>
                                 <p className={classes.service__content_text}>
                                     Фотосессия "Деловой стиль"
                                 </p>
