ee0b1aa02d488b411e468600d883084d537060a7
Yehor Haponov <egoriks1994@gmail.com>
2023-02-22 01:58:43 +0200

    refactor: Divide to CardItem.jsx

--- src/features/StageWorks/StageWorks.jsx
+++ src/features/StageWorks/StageWorks.jsx
@@ -1,69 +1,60 @@
 import React, {Component} from 'react';
 import classes from "./StageWorks.module.scss";
 import Card from "../../ui/components/Card/Card";
+import CardItem from "./CardItem/CardItem";
+
+const initialState = {
+    initialValues: [
+        {
+            title: 'Заявка',
+            text: 'Съешь еще этих мягких французских булок, да выпей чаю.\n Съешь еще этих мягких.',
+            number: '1'
+        },
+        {
+            title: 'Бронь',
+            text: 'Съешь еще этих мягких французских булок, да выпей чаю.\n Съешь еще этих мягких.',
+            number: '2'
+        },
+        {
+            title: 'Составление\n Технического задания',
+            text: 'Съешь еще этих мягких французских булок, да выпей чаю.\n Съешь еще этих мягких.',
+            number: '3'
+        },
+        {
+            title: 'Съёмка',
+            text: 'Съешь еще этих мягких французских булок, да выпей чаю.\n Съешь еще этих мягких.',
+            number: '4'
+        },
+        {
+            title: 'Готовые фото',
+            text: 'Съешь еще этих мягких французских булок, да выпей чаю.\n Съешь еще этих мягких.',
+            number: '5'
+        },
+    ]
+}
 
 class StageWorks extends Component {
+    constructor(props) {
+        super(props);
+        this.state = initialState;
+    }
+
     render() {
+        const {initialValues} = this.state;
         return (
             <section className={classes.stageWorks}>
                 <div className={classes.stageWorks__container}>
                     <h2 className={`${classes.stageWorks__title} _h2_title`}>Этапы работ</h2>
                     <div className={classes.stageWorks__body}>
                         <ul className={classes.stageWorks__body_container}>
-                            <Card className={`${classes.stageWorks__item} ${classes.item_content}`}>
-                                <div className={classes.item_content__wrapper}>
-                                    <h6 className={classes.item_content__title}>Заявка</h6>
-                                    <p className={classes.item_content__text}>
-                                        Съешь еще этих мягких французских булок, да выпей чаю.
-                                        Съешь еще этих мягких.
-                                    </p>
-                                    <p className={classes.item_content__number}>1</p>
-                                </div>
-                            </Card>
-                            <Card className={`${classes.stageWorks__item} ${classes.item_content}`}>
-                                <div className={classes.item_content__wrapper}>
-                                    <h6 className={classes.item_content__title}>Бронь</h6>
-                                    <p className={classes.item_content__text}>
-                                        Съешь еще этих мягких французских булок, да выпей чаю.
-                                        Съешь еще этих мягких.
-                                    </p>
-                                    <p className={classes.item_content__number}>2</p>
-                                </div>
-                            </Card>
-                            <Card className={`${classes.stageWorks__item} ${classes.item_content}`}>
-                                <div className={classes.item_content__wrapper}>
-                                    <h6 className={classes.item_content__title}>
-                                        Составление
-                                        <br/>
-                                        Технического задания
-                                    </h6>
-                                    <p className={classes.item_content__text}>
-                                        Съешь еще этих мягких французских булок, да выпей чаю.
-                                        Съешь еще этих мягких.
-                                    </p>
-                                    <p className={classes.item_content__number}>3</p>
-                                </div>
-                            </Card>
-                            <Card className={`${classes.stageWorks__item} ${classes.item_content}`}>
-                                <div className={classes.item_content__wrapper}>
-                                    <h6 className={classes.item_content__title}>Съёмка</h6>
-                                    <p className={classes.item_content__text}>
-                                        Съешь еще этих мягких французских булок, да выпей чаю.
-                                        Съешь еще этих мягких.
-                                    </p>
-                                    <p className={classes.item_content__number}>4</p>
-                                </div>
-                            </Card>
-                            <Card className={`${classes.stageWorks__item} ${classes.item_content}`}>
-                                <div className={classes.item_content__wrapper}>
-                                    <h6 className={classes.item_content__title}>Готовые фото</h6>
-                                    <p className={classes.item_content__text}>
-                                        Съешь еще этих мягких французских булок, да выпей чаю.
-                                        Съешь еще этих мягких.
-                                    </p>
-                                    <p className={classes.item_content__number}>5</p>
-                                </div>
-                            </Card>
+                            {initialValues.map(item =>
+                                <CardItem
+                                    title={item.title}
+                                    text={item.text}
+                                    number={item.number}
+                                    key={item.number + item.text}
+
+                                />)}
                         </ul>
                     </div>
                 </div>
