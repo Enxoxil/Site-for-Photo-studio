2136a657747d58cb50e27eeee414f2b234fc9ae4
Yehor Haponov <egoriks1994@gmail.com>
2023-03-11 17:40:44 +0200

    refactor: add children

--- src/ui/components/Popup/Popup.js
+++ src/ui/components/Popup/Popup.js
@@ -1,15 +1,18 @@
-import React, {Component} from 'react';
+import React, {cloneElement, Component} from 'react';
 import {createPortal} from "react-dom";
 import Backdrop from "./Backdrop/Backdrop";
 
 class Popup extends Component {
     render() {
         const {toggleShowPopup, isShowPopup} = this.props;
+        const children = cloneElement(this.props.children, {
+            toggleShowPopup, isShowPopup
+        });
         return (
             <>
                 {isShowPopup && createPortal( <Backdrop
                     toggleShowPopup={toggleShowPopup}/>, document.querySelector('#backdrop-root'))};
-                {isShowPopup && createPortal(this.props.children, document.querySelector('#modal-root'))}
+                {isShowPopup && createPortal(children, document.querySelector('#modal-root'))}
             </>
         );
     }
