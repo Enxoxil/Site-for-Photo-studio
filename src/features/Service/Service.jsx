d464e0eb4e01f68e9783424c64f1cfba9a7f1be5
Yehor Haponov <egoriks1994@gmail.com>
2023-03-17 00:12:24 +0200

    fix: refactor logic DND

--- src/features/Service/Service.jsx
+++ src/features/Service/Service.jsx
@@ -3,21 +3,17 @@ import classes from "./Service.module.scss";
 import Card from "../../ui/components/Card/Card";
 import Image from "../../ui/components/Image/Image";
 
-//import images
-import service_img1 from '../../assets/img/service-img1.png'
-import service_img2 from '../../assets/img/service-img2.png'
-import service_img3 from '../../assets/img/service-img3.png'
-import service_img4 from '../../assets/img/service-img4.png'
-import service_img5 from '../../assets/img/service-img5.png'
-import service_img6 from '../../assets/img/service-img6.png'
-import service_img7 from '../../assets/img/service-img7.png'
-import service_img8 from '../../assets/img/service-img8.png'
-import service_img9 from '../../assets/img/service-img9.png'
-import service_img10 from '../../assets/img/service-img10.png'
-import service_img11 from '../../assets/img/service-img11.png'
 
 class Service extends Component {
     render() {
+        const sort = (a, b) => {
+            if (a.id > b.id){
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+        const {cards, dropHandler, dragOverHandler, dragEndHandler, dragLeaveHandler, dragStartHandler} = this.props;
         return (
             <section className={classes.service}>
                 <div className={classes.service__container}>
@@ -25,83 +21,24 @@ class Service extends Component {
                         <h2 className={`${classes.service__title} _h2_title _scrollTo`} id="service__title">Виды
                             услуг</h2>
                         <ul className={classes.service__content_box}>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img1} alt="Фотосессия Лав Стори"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Фотосессия Лав Стори
-                                </p>
-                            </Card>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img2} alt="Фотосессия для подростков"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Фотосессия для подростков
-                                </p>
-                            </Card>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img3} alt="Парная фотосессия"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Парная фотосессия
-                                </p>
-                            </Card>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img4} alt="Фотосессия для парней"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Фотосессия для парней
-                                </p>
-                            </Card>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img5} alt="Семейная фотосессия (дети +14)"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Семейная фотосессия (дети +14)
-                                </p>
-                            </Card>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img6} alt="Групповая фотосессия"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Групповая фотосессия
-                                </p>
-                            </Card>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img7} alt="Фотосессия Лук Бук"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Фотосессия для Лук Бук
-                                </p>
-                            </Card>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img8} alt="Фотосессия для девушек"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Фотосессия для девушек
-                                </p>
-                            </Card>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img9} alt="Фотосессия для беременных"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Фотосессия для беременных
-                                </p>
-                            </Card>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img10} alt="Фотосессия с животными"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Фотосессия с животными
-                                </p>
-                            </Card>
-                            <Card className={classes.service__content_item}>
-                                <Image src={service_img11} alt="Фотосессия 'Деловой стиль'"
-                                       className={classes.service__content_img}/>
-                                <p className={classes.service__content_text}>
-                                    Фотосессия "Деловой стиль"
-                                </p>
-                            </Card>
+                            {cards.sort(sort).map((item) =>
+                                <Card
+                                    key={item.name}
+                                    className={classes.service__content_item}
+                                    draggable={true}
+                                    onDragStart={(e) => dragStartHandler(e, item)}
+                                    onDragLeave={(e) => dragLeaveHandler(e)}
+                                    onDragEnd={(e) => dragEndHandler(e)}
+                                    onDragOver={(e) => dragOverHandler(e)}
+                                    onDrop={(e) => dropHandler(e, item)}
+                                >
+                                    <Image src={item.img} alt={item.name}
+                                           className={classes.service__content_img}/>
+                                    <p className={classes.service__content_text}>
+                                        {item.name}
+                                    </p>
+                                </Card>
+                            )}
                         </ul>
                     </div>
                 </div>
---
+++ src/features/Service/ServiceContainer.js
@@ -0,0 +1,92 @@
+import React, {Component} from 'react';
+
+//import images
+import service_img1 from '../../assets/img/service-img1.png'
+import service_img2 from '../../assets/img/service-img2.png'
+import service_img3 from '../../assets/img/service-img3.png'
+import service_img4 from '../../assets/img/service-img4.png'
+import service_img5 from '../../assets/img/service-img5.png'
+import service_img6 from '../../assets/img/service-img6.png'
+import service_img7 from '../../assets/img/service-img7.png'
+import service_img8 from '../../assets/img/service-img8.png'
+import service_img9 from '../../assets/img/service-img9.png'
+import service_img10 from '../../assets/img/service-img10.png'
+import service_img11 from '../../assets/img/service-img11.png'
+import Service from "./Service";
+
+class ServiceContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: [
+                {id: 1, name: 'Фотосессия Лав Стори', img: service_img1},
+                {id: 2, name: 'Фотосессия для подростков', img: service_img2},
+                {id: 3, name: 'Парная фотосессия', img: service_img3},
+                {id: 4, name: 'Фотосессия для парней', img: service_img4},
+                {id: 5, name: 'Семейная фотосессия (дети +14)', img: service_img5},
+                {id: 6, name: 'Групповая фотосессия', img: service_img6},
+                {id: 7, name: 'Фотосессия Лук Бук', img: service_img7},
+                {id: 8, name: 'Фотосессия для девушек', img: service_img8},
+                {id: 9, name: 'Фотосессия для беременных', img: service_img9},
+                {id: 10, name: 'Фотосессия с животными', img: service_img10},
+                {id: 11, name: 'Фотосессия "Деловой стиль"', img: service_img11},
+            ],
+            currentCardId: '',
+        }
+        this.dropHandler = this.dropHandler.bind(this)
+        this.dragOverHandler = this.dragOverHandler.bind(this)
+        this.dragEndHandler = this.dragEndHandler.bind(this)
+        this.dragLeaveHandler = this.dragLeaveHandler.bind(this)
+        this.dragStartHandler = this.dragStartHandler.bind(this)
+    }
+
+    dropHandler(e, card) {
+        e.preventDefault();
+        const {cards, currentCardId} = this.state;
+        this.setState({
+                ...this.state,
+                cards: [...cards.map(item => {
+                    if (item.id === card.id) {
+                        return {...item, id: currentCardId};
+
+                    }
+                    if (item.id === currentCardId) {
+                        return {...item, id: card.id}
+                    }
+                    return item;
+                })]
+            }
+        )
+    }
+
+    dragOverHandler(e) {
+        e.preventDefault()
+    }
+
+    dragEndHandler(e) {
+        e.preventDefault();
+    }
+
+    dragLeaveHandler(e) {
+
+    }
+
+    dragStartHandler(e, card) {
+        this.setState({
+            currentCardId: card.id
+        })
+    }
+
+    render() {
+        const {cards} = this.state;
+        return <Service cards={cards}
+                        dropHandler={this.dropHandler}
+                        dragOverHandler={this.dragOverHandler}
+                        dragEndHandler={this.dragEndHandler}
+                        dragLeaveHandler={this.dragLeaveHandler}
+                        dragStartHandler={this.dragStartHandler}
+        />
+    }
+}
+
+export default ServiceContainer;
