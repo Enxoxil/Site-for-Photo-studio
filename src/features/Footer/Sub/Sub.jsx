8f6ee908fc3fbc48a7ee1527ce34f0e07788b264
Yehor Haponov <egoriks1994@gmail.com>
2023-04-24 08:32:00 +0300

    refactor: <Sub> migration to func component

--- src/features/Footer/Sub/Sub.jsx
+++ src/features/Footer/Sub/Sub.jsx
@@ -1,32 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Sub.module.scss'
 import SubItem from "./SubItem/SubItem";
 
-const initial_value = {
-    sub: [
-        {
-            link: 'https://www.instagram.com',
-            icon: '_icon_insta',
-            name: 'sub__inst'
-        },
-        {
-            link: 'https://facebook.com',
-            icon: '_icon_facebook',
-            name: 'sub__facebook'
-        },
-        {
-            link: 'https://twitter.com',
-            icon: '_icon_twitter',
-            name: 'sub__twitter'
-        },
-        {
-            link: 'https://web.telegram.org',
-            icon: '_icon_telegram',
-            name: 'sub__telegramm'
-        },
-
-    ]
-}
+const initial_value = [
+    {
+        link: 'https://www.instagram.com',
+        icon: '_icon_insta',
+        name: 'sub__inst'
+    },
+    {
+        link: 'https://facebook.com',
+        icon: '_icon_facebook',
+        name: 'sub__facebook'
+    },
+    {
+        link: 'https://twitter.com',
+        icon: '_icon_twitter',
+        name: 'sub__twitter'
+    },
+    {
+        link: 'https://web.telegram.org',
+        icon: '_icon_telegram',
+        name: 'sub__telegramm'
+    },
+]
 
 const Sub = () => {
     const [sub, setSub] = useState(initial_value)
