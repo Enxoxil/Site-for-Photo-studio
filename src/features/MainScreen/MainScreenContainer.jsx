3be9d7f27ede87109ea48c7ff039fc5f4870330e
Yehor Haponov <egoriks1994@gmail.com>
2023-04-24 08:28:54 +0300

    refactor: <MainScreenContainer> migration to func component

--- src/features/MainScreen/MainScreenContainer.jsx
+++ src/features/MainScreen/MainScreenContainer.jsx
@@ -1,32 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import withPopup from "../../HOC/WithPopup";
 import MainScreen from "./MainScreen";
 import Popup from "../../ui/components/Popup/Popup";
 import MainPopup from "../Popups/MainPopup/MainPopup";
 import {api} from "../../DAL/Api/api";
 
-class MainScreenContainer extends Component {
-
-    formHandler = (data) => {
-        this.setState({...this.state, data});
-        this.props.toggleShowPopup();
+const MainScreenContainer = ({toggleShowPopup, isShowPopup}) => {
+    const formHandler = (data) => {
+        toggleShowPopup();
         api.saveUser(data);
         console.log('Thank you! Your data was send to Firebase :', data)
     }
 
-    render() {
-        //Props will take HOC
-        const {toggleShowPopup, isShowPopup} = this.props;
-        return (
-            <>
-                <MainScreen toggleShowPopup={toggleShowPopup}/>
-                <Popup toggleShowPopup={toggleShowPopup} isShowPopup={isShowPopup}>
-                    <MainPopup formHandler={this.formHandler}/>
-                </Popup>
-            </>
-        );
-    }
+    return (
+        <>
+            <MainScreen toggleShowPopup={toggleShowPopup}/>
+            <Popup toggleShowPopup={toggleShowPopup} isShowPopup={isShowPopup}>
+                <MainPopup formHandler={formHandler}/>
+            </Popup>
+        </>
+    );
 }
 
-//HOC withPopup will add state for argument (component) method closure
 export default withPopup(MainScreenContainer);
