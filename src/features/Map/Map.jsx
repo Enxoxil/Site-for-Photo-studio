001e01ea0c3608998c07e31ced24b00260dc02f9
Yehor Haponov <egoriks1994@gmail.com>
2023-04-24 08:29:03 +0300

    refactor: <Map> migration to func component

--- src/features/Map/Map.jsx
+++ src/features/Map/Map.jsx
@@ -1,23 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
 import classes from './Map.module.scss'
 import Location from "./Location/Location";
 import MapForm from "./MapForm/MapForm";
 
-class Map extends Component {
-    render() {
-        return (<section className={`${classes.map} _scrollTo`}>
-                <div className={`${classes.map__container} _container`} id="map__wrapper">
-                    <div className={classes.map__wrapper}>
-                        <div className={classes.map__body}>
-                            <Location/>
-                            <MapForm/>
-                        </div>
-                    </div>
+const Map = () => (
+    <section className={`${classes.map} _scrollTo`}>
+        <div className={`${classes.map__container} _container`} id="map__wrapper">
+            <div className={classes.map__wrapper}>
+                <div className={classes.map__body}>
+                    <Location/>
+                    <MapForm/>
                 </div>
-            </section>
+            </div>
+        </div>
+    </section>
 
-        );
-    }
-}
+);
 
 export default Map;
