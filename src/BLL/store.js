32dc2e0785cd737645fbbd2834005ad6b1063093
Yehor Haponov <egoriks1994@gmail.com>
2023-04-27 01:20:24 +0300

    feat: create Store

---
+++ src/BLL/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from "@reduxjs/toolkit";
+import formReducer from "./reducers/form/form.reducer";
+
+const store = configureStore({
+    reducer: {formReducer}
+});
+
+export default store;
