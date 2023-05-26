ed164ffb460694cab148c3cb962b390d7fee4cdf
Yehor Haponov <egoriks1994@gmail.com>
2023-05-26 16:25:45 +0300

    feat: Create locales files

---
+++ src/i18n/locales/en/translation.js
@@ -0,0 +1,159 @@
+export default {
+  headerSection: {
+    nav: {
+      portfolio: 'Portfolio',
+      jobs: 'Types of jobs',
+      about: 'About us',
+      price: 'Services and prices',
+      contacts: 'Contacts',
+      review: 'Review'
+    },
+    button: {
+      themeBtn: 'Theme',
+      nameBtn: 'Feedback'
+    }
+  },
+  mainContentSection: {
+    h1: '<span>Professional</span> photo session in the studio',
+    sectionTitle: 'Our photo studio will make your dreams come true',
+    content: `Photos have become an integral part of our lives.
+              <br />
+              Now these are not just memorable pictures in albums, now the photo becomes
+              way to please others, to get admiration,
+              declare yourself to the whole world.
+              <br />
+              Choosing the right photo studio is far from the last place in getting
+              photos. And we declare with certainty that you will remain our photo studio
+              satisfied and want to come back to us again and again.`,
+    mainBtn: 'Get a consultation'
+  },
+  ourWorksSection: {
+    sectionTitle: 'Our works',
+  },
+  designStudioSection: {
+    sectionTitle: 'Photo studio design'
+  },
+  servicesSection: {
+    sectionTitle: 'Services',
+    cardsTitle: {
+      id_1: 'Photoshoot "Love story"',
+      id_2: 'Photoshoot for teenagers',
+      id_3: 'Pair photoshoot',
+      id_4: 'Photoshoot for man',
+      id_5: 'Family photoshoot (children 14+)',
+      id_6: 'Group  photoshoot',
+      id_7: 'Photoshoot "Look Book"',
+      id_8: 'Photoshoot for woman',
+      id_9: 'Photoshoot for pregnant',
+      id_10: 'Photoshoot with animals',
+      id_11: 'Photoshoot "Business style"',
+    }
+  },
+  stageWorksSection: {
+    sectionTitle: 'Stage of works',
+    cards: {
+      number_1: {
+        title: 'Application',
+        content: 'Eat some more of those soft French buns and have some tea. Eat more of these soft.',
+      },
+      number_2: {
+        title: 'Booking',
+        content: 'Eat some more of those soft French buns and have some tea. Eat more of these soft.',
+      },
+      number_3: {
+        title: 'Preparation of terms of reference',
+        content: 'Eat some more of those soft French buns and have some tea. Eat more of these soft.',
+      },
+      number_4: {
+        title: 'Shooting',
+        content: 'Eat some more of those soft French buns and have some tea. Eat more of these soft.',
+
+      },
+      number_5: {
+        title: 'Finish',
+        content: 'Eat some more of those soft French buns and have some tea. Eat more of these soft.',
+      },
+
+    }
+  },
+  priceSection: {
+    sectionTitle: 'The cost of a photo session',
+    cards: {
+      mini: {
+        name: 'MINI',
+        valueOfLocations: '/ 1 location',
+        options: {
+          optionsItem_1: ' Duration — 30 min',
+          optionsItem_2: ' Sources — up to 100 pcs',
+          optionsItem_3: ' Retouch - 4 photo',
+          optionsItem_4: ' Studio rental - free of charge',
+          optionsItem_5: ' Any image',
+        }
+      },
+      standart: {
+        name: 'STANDART',
+        valueOfLocations: '/ 2 locations',
+        options: {
+          optionsItem_1: ' Duration - 1 hour 20 min',
+          optionsItem_2: ' Sources - up to 200 pcs',
+          optionsItem_3: ' Retouch - 8 photo',
+          optionsItem_4: ' Studio rental - free of charge',
+          optionsItem_5: ' Any image',
+        }
+      },
+      maxi: {
+        name: 'MAXI',
+        valueOfLocations: '/ All locations',
+        options: {
+          optionsItem_1: ' Duration - 4 hours',
+          optionsItem_2: ' Sources - up to 500 pcs',
+          optionsItem_3: ' Retouch - 50 photo',
+          optionsItem_4: ' Studio rental - free of charge',
+          optionsItem_5: ' Visagist - Make-Up, Laying',
+          optionsItem_6: ' Preliminary selection of images - Up to 8 images',
+        }
+      },
+    },
+    btnOptions: {
+      btnOptionsTitle: 'Additional services',
+      btnOptions_1: 'Help<br /> with posing',
+      btnOptions_2: 'Visagist<br /> + 2000<span>₴</span>',
+      btnOptions_3: 'Additional photo<br /> retouching - 1pcs/250<span>₴</span>',
+    },
+    btnName: 'Book'
+  },
+  giftsSection: {
+    sectionTitle: 'Gift certificate for<br /> a photo session',
+    card: {
+      cardPreTitle: 'CERTIFICATE FOR PHOTO SESSION',
+      cardTitle: 'This is a gift that sinks into the soul',
+      cardList: {
+        listItem_1: 'Creative design',
+        listItem_2: 'Open calendar',
+        listItem_3: 'All inclusive',
+      },
+      cardText: `Certificate validity period: 2 months <br />
+                 from the date of purchase.`,
+      cardBtnName: 'Buy'
+    }
+  },
+  mapSection: {
+    popoverText: '<span>Working mode</span><br/>',
+    popoverTime: 'Mon-Fri 09:00 - 18:00',
+  },
+  footerSection: {
+    footerText: 'Our social networks: ',
+  },
+  popupCallback: {
+    title: 'Feedback form',
+    nameLabel: 'Name',
+    nameField: 'Enter your name',
+    emailLabel: 'E-mail',
+    emailField: 'Enter your E-mail',
+    telLabel: 'Phone',
+    telField: 'Enter your phone',
+    nameBtn: 'Submit',
+    policyText: 'By clicking the "Submit" button, you consent to the processing of personal data and agree to ',
+    policy: 'privacy policy'
+  }
+};
