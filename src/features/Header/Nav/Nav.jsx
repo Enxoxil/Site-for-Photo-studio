520de248e4a8b3bd09c52403077bda6e77f94553
Yehor Haponov <egoriks1994@gmail.com>
2023-05-26 16:32:01 +0300

    refactor: use i18next in Nav.jsx

--- src/features/Header/Nav/Nav.jsx
+++ src/features/Header/Nav/Nav.jsx
@@ -1,30 +1,33 @@
 import React, { useState } from 'react';
 import cn from 'classnames';
+import { Trans, useTranslation } from 'react-i18next';
 import classes from './Nav.module.scss';
 import NavLink from './NavLink/NavLink';
 import Button from '../../../ui/components/Button/Button';
 import { useTheme } from '../../../hooks/useTheme';
+import {
+  DESIGN_STUDIO_TITLE_LINK, MAP_TITLE_LINK,
+  OUR_WORKS_TITLE_LINK,
+  PRICE_TITLE_LINK,
+  SERVICE_TITLE_LINK
+} from '../../../const/const';
 
 const initialValue = [
   {
-    link: '#ourWorks__title',
-    name: 'Портфолио'
-  },
-  {
-    link: '#service__title',
-    name: 'Виды работ'
-  },
-  {
-    link: '#designStudio__title',
-    name: 'О фотостудии'
-  },
-  {
-    link: '#price__title',
-    name: 'Услуги и цены'
-  },
-  {
-    link: '#map__wrapper',
-    name: 'Контакты'
+    link: OUR_WORKS_TITLE_LINK,
+    name: 'headerSection.nav.portfolio',
+  }, {
+    link: SERVICE_TITLE_LINK,
+    name: 'headerSection.nav.jobs'
+  }, {
+    link: DESIGN_STUDIO_TITLE_LINK,
+    name: 'headerSection.nav.about'
+  }, {
+    link: PRICE_TITLE_LINK,
+    name: 'headerSection.nav.price'
+  }, {
+    link: MAP_TITLE_LINK,
+    name: 'headerSection.nav.contacts'
   },
 ];
 
@@ -32,6 +35,7 @@ const Nav = ({
   isShowBurger,
   toggleBurger
 }) => {
+  const { t } = useTranslation();
   const [nav] = useState(initialValue);
   const { toggleThemeHandler } = useTheme();
   const btnThemeHandler = () => {
@@ -49,12 +53,14 @@ const Nav = ({
                       <NavLink
                         key={item.link}
                         link={item.link}
-                        name={item.name}
+                        name={t(item.name)}
                         toggleBurger={toggleBurger}
                       />
                   ))}
                   <div className={classes.menu__theme_btn_container}>
-                      <Button className={classes.menu__theme_btn} onClick={btnThemeHandler}>Тема</Button>
+                      <Button className={classes.menu__theme_btn} onClick={btnThemeHandler}>
+                          <Trans i18nKey="headerSection.button.themeBtn" />
+                      </Button>
                   </div>
               </ul>
           </nav>
