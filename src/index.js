a3714422c9bacece5e0f5ff1d59c751fb7c07be3
Yehor Haponov <egoriks1994@gmail.com>
2023-05-26 16:27:12 +0300

    feat: use Provider & suspense for i18next

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import { I18nextProvider } from 'react-i18next';
 import App from './App';
+import i18next from './i18n/i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <I18nextProvider i18n={i18next}>
+        <Suspense fallback={<div>Loading...</div>}>
+            <App />
+        </Suspense>
+    </I18nextProvider> 
 );
