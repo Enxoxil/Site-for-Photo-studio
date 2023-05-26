945b21ccf2c35390afcf9a921e65019df2c41274
Yehor Haponov <egoriks1994@gmail.com>
2023-05-26 16:33:50 +0300

    refactor: use i18next in SubItem.jsx

--- src/features/Footer/Sub/SubItem/SubItem.jsx
+++ src/features/Footer/Sub/SubItem/SubItem.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import classes from './SubItem.module.scss';
 
 const SubItem = (props) => {
-  const { link, name, icon } = props;
+  const { link, icon } = props;
 
-  return <a href={link} className={`${name} ${icon} ${classes._sub_icon}`} />;
+  return <a href={link} className={`${icon.name} ${icon.icon} ${classes._sub_icon}`} />;
 };
 
 export default SubItem;
