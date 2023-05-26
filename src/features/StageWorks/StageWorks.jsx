2edbde85eeb4f1fd6fd3fecc7c8d64b9ed71db7b
Yehor Haponov <egoriks1994@gmail.com>
2023-05-26 16:33:24 +0300

    refactor: use i18next in StageWorks.jsx

--- src/features/StageWorks/StageWorks.jsx
+++ src/features/StageWorks/StageWorks.jsx
@@ -1,50 +1,53 @@
 import React, { useState } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 import classes from './StageWorks.module.scss';
 import CardItem from './CardItem/CardItem';
 
 const initialState = [
   {
-    title: 'Заявка',
-    text: 'Съешь еще этих мягких французских булок, да выпей чаю.\n Съешь еще этих мягких.',
+    title: 'stageWorksSection.cards.number_1.title',
+    text: 'stageWorksSection.cards.number_1.content',
     number: '1'
   },
   {
-    title: 'Бронь',
-    text: 'Съешь еще этих мягких французских булок, да выпей чаю.\n Съешь еще этих мягких.',
+    title: 'stageWorksSection.cards.number_2.title',
+    text: 'stageWorksSection.cards.number_2.content',
     number: '2'
   },
   {
-    title: 'Составление\n Технического задания',
-    text: 'Съешь еще этих мягких французских булок, да выпей чаю.\n Съешь еще этих мягких.',
+    title: 'stageWorksSection.cards.number_3.title',
+    text: 'stageWorksSection.cards.number_3.content',
     number: '3'
   },
   {
-    title: 'Съёмка',
-    text: 'Съешь еще этих мягких французских булок, да выпей чаю.\n Съешь еще этих мягких.',
+    title: 'stageWorksSection.cards.number_4.title',
+    text: 'stageWorksSection.cards.number_4.content',
     number: '4'
   },
   {
-    title: 'Готовые фото',
-    text: 'Съешь еще этих мягких французских булок, да выпей чаю.\n Съешь еще этих мягких.',
+    title: 'stageWorksSection.cards.number_5.title',
+    text: 'stageWorksSection.cards.number_5.content',
     number: '5'
   },
 ];
 
 const StageWorks = () => {
   const [cards,] = useState(initialState);
-
+  const { t } = useTranslation();
   return (
       <section className={classes.stageWorks}>
           <div className={classes.stageWorks__container}>
-              <h2 className={`${classes.stageWorks__title} _h2_title`}>Этапы работ</h2>
+              <h2 className={`${classes.stageWorks__title} _h2_title`}>
+                  <Trans i18nKey="stageWorksSection.sectionTitle" />
+              </h2>
               <div className={classes.stageWorks__body}>
                   <ul className={classes.stageWorks__body_container}>
                       {cards.map((item) => (
                           <CardItem
-                            title={item.title}
-                            text={item.text}
+                            title={t(item.title)}
+                            text={t(item.text)}
                             number={item.number}
-                            key={item.number + item.text}
+                            key={item.text}
                           />
                       ))}
                   </ul>
