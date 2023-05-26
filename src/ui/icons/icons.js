655859a896112e6f26824a5b76519f18ff7ef567
Yehor Haponov <egoriks1994@gmail.com>
2023-05-26 16:28:55 +0300

    refactor: create consts for icons

---
+++ src/ui/icons/icons.js
@@ -0,0 +1,16 @@
+export const INSTAGRAM_ICON = {
+  icon: '_icon_insta',
+  name: 'sub__inst'
+};
+export const FACEBOOK_ICON = {
+  icon: '_icon_facebook',
+  name: 'sub__facebook'
+};
+export const TWITTER_ICON = {
+  icon: '_icon_twitter',
+  name: 'sub__twitter'
+};
+export const TELEGRAM_ICON = {
+  icon: '_icon_telegram',
+  name: 'sub__telegramm'
+};
