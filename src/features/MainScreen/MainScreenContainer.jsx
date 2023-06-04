0afbdd8405fa627749388a0820a5d86ca12b46f0
Yehor Haponov <egoriks1994@gmail.com>
2023-06-04 23:51:02 +0300

    refactor: use formApi hook

--- src/features/MainScreen/MainScreenContainer.jsx
+++ src/features/MainScreen/MainScreenContainer.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 import MainScreen from './MainScreen';
 import Popup from '../../ui/components/Popup/Popup';
 import MainPopup from '../Popups/MainPopup/MainPopup';
-import { sendClient } from '../../BLL/reducers/form/form.reducer';
+// import { sendClient } from '../../BLL/slices/formApi (old ver. for RTK)/formApi (old ver. for RTK).reducer';
 import { usePopup } from '../../hooks/usePopup';
+import { useAddNewUserMutation } from '../../BLL/slices/form/apis/AddNewUserApi';
 
 const MainScreenContainer = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const { isShowPopup, toggleShowPopup } = usePopup();
+  const [addUser] = useAddNewUserMutation();
   const formHandler = (data) => {
-    dispatch(sendClient(data));
+    addUser(data);
+    // dispatch(sendClient(data));
     toggleShowPopup();
     console.log('Thank you! Your data was send to Firebase :', data);
   };
